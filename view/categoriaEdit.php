c16cbee0d0565429bdb00ed4d4df86ce1b083ee5
Allan Robinson Parkinson <allanrp0110@gmail.com>
2024-09-17 00:38:23 -0600

    changes

---
+++ view/categoriaEdit.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form id="createForm" method="post" enctype="multipart/form-data" action="../business/categoriaActions.php">
+            <?php
+            require_once '../business/categoriaBusiness.php';
+            require_once '..domain/categoria.php';
+
+            $categoriaBusiness = new categoriaBusiness();
+
+            if (isset($_GET['id'])) {
+                $categoria = $categoriaBusiness->getCategoria($_GET['id']);
+            }
+
+            echo "<input type='hidden' name='id' value='" . $categoria->getIdCategoria() . "'>"; 
+
+            echo "<label for='nombreCategoria'>Nombre de la categoria</label><br>";
+            echo "<input type='text' name='nombre' value='" . $categoria->getNombreCategoria() . "'> <br>";
+            ?>
+            <label for="estado">Estado</label>
+            <select name="estado">
+                <option value="1">Activo</option>
+                <option value="0">Inactivo</option>
+            </select>
+
+
+        <input type='submit' value='Actualizar' name='update'>
+        </form>
+
+    </div>
+    
+</body>
+</html>
---
+++ view/instrumentosEdit.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form id="createInstrumentoForm" method="post" action="../business/instrumentosActions.php">
+            <?php
+            require_once '../business/instrumentosBusiness.php';
+            require_once '..domain/instrumentos.php';
+
+            $instrumentosBusiness = new instrumentosBusiness();
+
+            if (isset($_GET['id'])) {
+                $instrumentos = $instrumentosBusiness->getInstrumento($_GET['id']);
+            }
+
+            echo "<input type='hidden' name='id' value='" . $instrumentos->getId() . "'>"; 
+
+            echo "<label for='nombreInstrumento'>Nombre del instrumento:</label><br>";
+            echo "<input type='text' name='nombreInstrumento' value='" . $instrumentos->getNombre() . "' required><br>";
+
+            echo "<label for='categoriaInstrumento'>Categoria del instrumento:</label><br>";
+            echo "<input type='text' name='categoriaInstrumento' value='" . $instrumentos->getCategoria() . "' required><br>";
+            
+            ?>
+            <input type="submit" value="Actualizar" name="update">
+        </form>
+    </div>
+    
+</body>
+</html>
